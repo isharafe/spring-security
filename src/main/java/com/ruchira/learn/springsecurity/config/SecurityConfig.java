63e81a78b8951c251e23d5a86990d0780cd4ca46
Ruchira <ruchira@coredevone.com>
2022-09-20 15:53:23 +0530

    in memory users with basic auth

--- src/main/java/com/ruchira/learn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/ruchira/learn/springsecurity/config/SecurityConfig.java
@@ -1,8 +1,18 @@
 package com.ruchira.learn.springsecurity.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +28,23 @@ public class SecurityConfig {
 					)
 		.httpBasic();
 		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		PasswordEncoder pswEncoder = passwordEncoder();
+		List<UserDetails> users = new ArrayList<>();
+		/**
+		 * Need to enter authorities. o/w you will get an exception on startup complaining
+		 * authorities are null
+		 */
+		users.add(User.builder().passwordEncoder(pswEncoder::encode).username("user").password("psw").authorities("USER", "DEV").build());
+		users.add(User.builder().passwordEncoder(pswEncoder::encode).username("test").password("test").authorities("Admin").build());
+		return new InMemoryUserDetailsManager(users);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
