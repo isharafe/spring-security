e9b787bd6d99329242ba70e224d27b901a82860e
Ruchira <ruchira@coredevone.com>
2022-09-20 20:54:32 +0530

    couldn't make this work so far. todo later...

--- src/main/java/com/ruchira/learn/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/ruchira/learn/springsecurity/config/SecurityConfig.java
@@ -2,7 +2,12 @@ package com.ruchira.learn.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.ldap.userdetails.PersonContextMapper;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +22,23 @@ public class SecurityConfig {
 				.anyRequest().permitAll()
 					)
 		.httpBasic();
+
 		return http.build();
+	}
+
+	@Bean
+    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() throws Exception {
+        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
+            EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
+        contextSourceFactoryBean.setPort(0);
+        return contextSourceFactoryBean;
+    }
+
+	@Bean
+	public AuthenticationManager ldapAuthenticationManager(BaseLdapPathContextSource contextSource) {
+		LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
+		factory.setUserDnPatterns("uid={0},ou=people");
+		factory.setUserDetailsContextMapper(new PersonContextMapper());
+		return factory.createAuthenticationManager();
 	}
 }
