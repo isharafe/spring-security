dab63921d94c1ff2ea6700b45f39b4be7969cdfb
Ishara <isharafe@gmail.com>
2023-05-13 16:06:13 +0530

    initial spring starter project

---
+++ src/test/java/my/learn/oidc/OidcApplicationTests.java
@@ -0,0 +1,13 @@
+package my.learn.oidc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
